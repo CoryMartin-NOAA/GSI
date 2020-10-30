d8bf7b11588a31de9758e33858ff0104197e0f91
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2020-10-30 19:07:54 +0000

    add pblh to setuprhsall

--- src/gsi/setuprhsall.f90
+++ src/gsi/setuprhsall.f90
@@ -119,7 +119,7 @@ subroutine setuprhsall(ndata,mype,init_pass,last_pass)
 !$$$
   use kinds, only: r_kind,i_kind,r_quad,r_single
   use constants, only: zero,one,fv,zero_quad
-  use guess_grids, only: load_prsges,load_geop_hgt,load_gsdpbl_hgt
+  use guess_grids, only: load_prsges,load_geop_hgt,load_gsdpbl_hgt,load_pbl_hgt
   use guess_grids, only: ges_tsen,nfldsig
   use obsmod, only: nsat1,iadate,&
        ndat,obs_setup,&
@@ -359,6 +359,8 @@ subroutine setuprhsall(ndata,mype,init_pass,last_pass)
       end if
    endif   
 
+! Compute 3d subdomain geopotential heights from the guess fields
+   call load_pbl_hgt(mype)
 
 ! Compute derived quantities on grid
   if(.not.cmaq_regional) call compute_derived(mype,init_pass)
